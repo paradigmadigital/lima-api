fe76ca726e0c1808b9b201ca739c4a740c020607
Victor Torre <web.ehooo@gmail.com>
2024-07-29 12:36:02 +0200

    Improve LimaException (#9)

--- src/lima_api/exceptions.py
+++ src/lima_api/exceptions.py
@@ -1,9 +1,28 @@
+from typing import Optional
+
+
 class LimaException(Exception):
-    def __init__(self, detail: str = "Error de Lima", status_code: int = 0, content: bytes = b""):
-        self.detail = detail
-        self.status_code = status_code
-        self.content = content
+    detail: str = "Error de Lima"
+    status_code: int = -1
+    content: bytes = b""
+
+    def __init__(
+        self,
+        *,
+        detail: Optional[str] = None,
+        status_code: Optional[int] = None,
+        content: Optional[bytes] = None,
+    ):
+        if detail is not None:
+            self.detail = detail
+        if status_code is not None:
+            self.status_code = status_code
+        if content is not None:
+            self.content = content
 
     def __repr__(self) -> str:
         class_name = self.__class__.__name__
         return f"{class_name}(detail={self.detail!r}, status_code={self.status_code!r})"
+
+    def __str__(self) -> str:
+        return self.detail
