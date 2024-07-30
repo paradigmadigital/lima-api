97e158045ef6ab2f43ce05a4868b5fb49e9250b9
Victor Torre <web.ehooo@gmail.com>
2024-07-30 09:57:54 +0200

    Add support for logging (#11)
    * Add support for logging

--- src/lima_api/__init__.py
+++ src/lima_api/__init__.py
@@ -1,6 +1,6 @@
 """Lima Framework, build async API rest clients"""
 
-from .core import LimaApi, SyncLimaApi  # noqa
+from .core import LimaApi, LogEvent, SyncLimaApi  # noqa
 from .core import get, post, put, head, patch, options, delete  # noqa
 from .exceptions import LimaException  # noqa
 from .parameters import BodyParameter, HeaderParameter, PathParameter, QueryParameter  # noqa
